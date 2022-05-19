ee922f1e436a7e4fd1d8b6220c3aca397210f5c6
tomiwa abiola <tomiwabiola02@gmail.com>
2022-05-20 02:13:50 +0800

    onboarding flow included

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
 document.querySelector('#signUpButton').addEventListener('click', congratulate);
-document.querySelector('#resubmit').addEventListener('click', formON)
+
 
 const formOff = document.querySelector('#formy');
 
 function congratulate(){
     let nameCollector = document.querySelector('#name').value;
-    const resubmitBtn = document.querySelector('#resubmit');
+    const thankYouPage = document.querySelector('#thankYouPage');
     document.querySelector('#congrats').innerText = nameCollector;
     formOff.classList.add('hidden')
-    resubmitBtn.classList.remove('hidden');
+    thankYouPage.classList.remove('hidden');
 
     console.log(nameCollector)
-}
-
-function formON(){
-const onForm = document.querySelector('#formy');
-const resubmitBtn = document.querySelector('#resubmit');
-onForm.classList.remove('hidden')
-// document.querySelector('#congrats').innerText = " ";
-resubmitBtn.classList.toggle('hidden');
-
-    
 }
